373d5f283106588669c1818f6fec843706f8f309
John Thompson <jdthompson007@gmail.com>
2017-10-04 10:00:52 +0100

    Added another test case

--- src/test/java/groceries/services/JsonServiceTest.java
+++ src/test/java/groceries/services/JsonServiceTest.java
@@ -48,4 +48,25 @@ public class JsonServiceTest {
 		// THEN the JSON result has the correct items with the correct total value
 		JSONAssert.assertEquals("{\"results\":[{\"title\":\"Strawberry\",\"description\":\"As found at wimbledon\",\"kcal_per_100g\":200,\"unit_price\":1.5},{\"title\":\"Blueberry\",\"description\":\"berries\",\"kcal_per_100g\":100,\"unit_price\":2.5}],\"total\":4.0}", json, false);
 	}	
+	
+	@Test
+	public void testConvertGroceriesToJsonMissingCalories() throws Exception {
+		// GIVEN we have a grocery item with no calorie value
+		List<Grocery> groceries = new ArrayList<Grocery>();
+		
+		Grocery grocery = new Grocery();
+		grocery.setTitle("Strawberry");
+		grocery.setPrice(new BigDecimal(1.50));
+		grocery.setDescription("As found at wimbledon");
+		groceries.add(grocery);
+		
+		GroceryResult groceryResult = new GroceryResult();
+		groceryResult.setGroceries(groceries);
+		
+		// WHEN the grocery item is converted to JSON
+		String json = jsonService.convertGroceryResultToJson(groceryResult);
+		
+		// THEN the JSON result has the correct item with the correct total value
+		JSONAssert.assertEquals("{\"results\":[{\"title\":\"Strawberry\",\"description\":\"As found at wimbledon\",\"unit_price\":1.5}],\"total\":1.50}", json, false);
+	}	
 }
